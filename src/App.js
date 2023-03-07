4b1f33f68e5328efce4f0489797abce451da757c
ciodeevolver <goodnessokanlawon12@gmail.com>
2023-03-07 21:23:24 +0100

    changed path name

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 
 //font
-import "../src/fonts/LeagueSpartan-VariableFont_wght.ttf";
+import "./fonts/LeagueSpartanVariableFontwght.ttf";
 
 class App extends React.Component {
   constructor(props){
