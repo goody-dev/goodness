b7c9945d8eef02dd4513268bb02941f1b62139e6
ciodeevolver <goodnessokanlawon12@gmail.com>
2023-03-08 15:39:42 +0100

    added new files

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Header } from './components/Header';
+export { default as About } from './About';
+export { default as WorkLife } from './WorkLife';
+export { default as Stack } from './Stack';
+export { default as Projects } from './Projects';
+export { default as Testimonials } from './Testimonials';
+export { default as Contact } from './Contact';
+export { default as Footer } from './Footer';
